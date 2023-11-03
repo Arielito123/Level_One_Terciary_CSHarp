c9c45b1a4bde1f2d61e1a4996be76b732f2dbda6
Arielito123 <104540300+Arielito123@users.noreply.github.com>
2023-11-03 15:22:29 -0300

    Tp0 de analisis de sistemas primer año
    Ejercicio 1

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1. Diseñar un Programa que permita visualizar en pantalla la leyenda “Este es mi
+ primer programa”.*/
+            Console.WriteLine("este es mi primer programa");
+            Console.ReadKey();
+        }
+    }
+}
+
